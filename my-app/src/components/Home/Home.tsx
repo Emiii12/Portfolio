4c191f75011b52628b9774d2d1aa576bfe77d7af
Emilio Pereira <91755231+Emiii12@users.noreply.github.com>
2025-02-28 10:25:39 -0300

    Home Section

--- my-app/src/components/Home/Home.tsx
+++ my-app/src/components/Home/Home.tsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import ImageWebP from '../../../public/Image/fotocv.webp'
  
 const Home = () => {
   return (
-    <section className="w-full h-full py-24 grid grid-cols-2 grid-rows-[3fr_1fr] gap-4">
-      <div className="bg-blue-500"></div>
-      <div className="flex flex-col gap-4">
-				<div className='w-full h-full bg-yellow-500'>
-
-				</div>
-				<div className='w-full h-full bg-yellow-500'>
-
-				</div>
-				<div className='w-full h-full bg-yellow-500'>
-
-				</div>
-			</div>
-      <div className="bg-green-500 col-span-2"></div>
+    <section className='w-full h-full py-24 '>
+      <div className='w-full h-full flex flex-col lg:flex-row justify-center items-center'>
+        <div className='order-last lg:order-first lg:w-1/2 w-full h-full'>
+          <h2 className='text-[22px] text-white font-semibold text-center'>
+            Desarrollador Web <span className='text-lightSeaGreen'>Front</span>
+            -
+            <span className='text-lightSeaGreen'>End</span>
+          </h2>
+          <p>Hola, soy Emilio Pereira. Un apasionado de la programación en Buenos Aires, Argentina.</p>
+          <div className='flex flex-col lg:flex-row'>
+            <button>
+              Proyectos
+            </button>
+            <button>
+              Ver CV
+            </button>
+            <button>
+              Contáctame
+            </button>
+          </div>
+        </div>
+        <div className='order-first lg:order-last lg:w-1/2 flex justify-center items-center'>
+          <div className='p-1 rounded-full border-[2px] border-vibrantOrange '>
+            <div className='w-auto h-auto lg:w-96 lg:h-96'>
+              <Image src={ImageWebP} alt='foto ejemplo' className='w-auto h-auto lg:w-full lg:h-full rounded-full'/>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
